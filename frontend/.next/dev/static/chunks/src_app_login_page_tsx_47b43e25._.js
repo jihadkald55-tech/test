3fd3611bd9d64c1cab4dc3e814f54aa82a2dc99d
jihadkald55-tech <jihadkald55@gmail.com>
2026-02-04 01:43:32 +0300

    Add frontend/.next/dev/static/chunks/src_app_login_page_tsx_47b43e25._.js

---
+++ frontend/.next/dev/static/chunks/src_app_login_page_tsx_47b43e25._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_14ced73b._.js",
+  "static/chunks/src_6721e0f5._.js"
+],
+    source: "dynamic"
+});
