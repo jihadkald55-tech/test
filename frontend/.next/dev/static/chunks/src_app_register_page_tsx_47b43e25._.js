416895f83b942dbcd5cde42399e50f272ab82b8f
jihadkald55-tech <jihadkald55@gmail.com>
2026-02-04 01:43:34 +0300

    Add frontend/.next/dev/static/chunks/src_app_register_page_tsx_47b43e25._.js

---
+++ frontend/.next/dev/static/chunks/src_app_register_page_tsx_47b43e25._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_14ced73b._.js",
+  "static/chunks/src_811105f9._.js"
+],
+    source: "dynamic"
+});
